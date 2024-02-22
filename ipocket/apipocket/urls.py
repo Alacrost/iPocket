fdd31df8f304dd8564e3697535ed90c22ccc77ca
Albert Bauermann <albert.baumer@gmail.com>
2024-02-22 18:26:18 -0300

    Create/Update/Delete DONE

--- ipocket/apipocket/urls.py
+++ ipocket/apipocket/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import lista_ativos, detalhes_ativo, novo_ativo, editar_ativo, excluir_ativo
 
 urlpatterns = [
-    path('ativos/', lista_ativos, name='lista_ativos'),
+    path('', lista_ativos, name='lista_ativos'),
     path('ativos/<int:pk>/', detalhes_ativo, name='detalhes_ativo'),
     path('ativos/novo/', novo_ativo, name='novo_ativo'),
     path('ativos/editar/<int:pk>/', editar_ativo, name='editar_ativo'),
--- ipocket/apipocket/views.py
+++ ipocket/apipocket/views.py
@@ -19,25 +19,26 @@ def novo_ativo(request):
         if form.is_valid():
             ativo = form.save(commit=False)
             ativo.save()
-            return redirect('lista_ativos.html')
+            return redirect('lista_ativos')
     else:
         form = AtivoForm()
     return render(request, 'novo_ativo.html', {'form': form})
 
 
 def editar_ativo(request, pk):
+
     ativo = get_object_or_404(Ativo, pk=pk)
     if request.method == 'POST':
         form = AtivoForm(request.POST, instance=ativo)
         if form.is_valid():
             form.save()
-            return redirect('lista_ativos.html')
+            return redirect('lista_ativos')
     else:
         form = AtivoForm(instance=ativo)
     return render(request, 'editar_ativo.html', {'form': form, 'ativo': ativo})
 
 
-def excluir_ativo(pk):
+def excluir_ativo(request, pk):
     ativo = get_object_or_404(Ativo, pk=pk)
     ativo.delete()
-    return redirect('lista_ativos.html')
+    return redirect('lista_ativos')
